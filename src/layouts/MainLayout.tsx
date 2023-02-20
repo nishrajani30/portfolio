a548b23d4b0c986646cfb991bd543728bbb162d2
nishrajani30 <nishrajani@gmail.com>
2023-02-21 07:13:37 +1030

    Fixed build

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,7 +7,7 @@ import Footer from '../components/Footer'
 import Header from '../components/Header'
 import SectionContainer from '../components/SectionContainer'
 import {ThemeProvider} from "next-themes";
-import {Analytics} from "@vercel/analytics/dist/react";
+import { Analytics } from '@vercel/analytics/react';
 
 interface Props {
   children: ReactNode
